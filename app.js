7b650c7bd6bc98f055fb0f1350650986e41c6d25
Grajaka <karojg24@gmail.com>
2021-03-23 20:56:21 -0500

    space

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 const express = require('express');
 const app = express();
-var name
-app.get('/', (req, res) => {
-  const name = req.query.nombre || 'desconocido';;
 
-  res.send(`<h1> Hola  ${name} ! </h1>`);
+app.get('/', (req, res) => {
+  const nombre = req.query.nombre || 'desconocido';;
+
+  res.send(`<h1>Hola ${nombre}! </h1>`);
 });
 
 
 
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
-
-/* var test = name || '';
-if((name ? name )){
-  name = "desconocido"
-}
-*/
