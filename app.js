d91d6d1deaab9f4f50b9b8d8a59873b701b2877d
Grajaka <karojg24@gmail.com>
2021-03-25 12:56:16 -0500

    Pares_e_impares

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
 
-app.get('/user/:nombre', (req, res) => {
-  const nombre = req.params.nombre || 'desconocido';;
-  const Capnombre = nombre.charAt(0).toUpperCase()+ nombre.slice(1);
-  res.send(`<h1>Hola ${Capnombre}!</h1>`);
+app.get('/', (req, res) => {
+  var i=1;
+  var text='';
+  var paragraph ='';
+  while (i<51){
+    if (i % 2 === 0){
+      text = i + ' Soy par!\n';
+    }
+    else{
+      text = i+ ' Soy impar\n';
+
+    }
+    i++;
+    paragraph += '<h1>' + text + '</h1>';
+
+  }
+res.send(`${paragraph}`);
+
 });
-
-
-
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
